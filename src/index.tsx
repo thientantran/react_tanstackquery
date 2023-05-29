a04e6e373f4be0e71f1bb74ed84a7a5931c8ad6b
thientantran <thientantm@gmail.com>
2023-05-29 22:15:03 +0700

    yarn add @tanstack/react-query-devtools

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
@@ -13,6 +14,7 @@ root.render(
     <BrowserRouter >
       <QueryClientProvider client={queryClient}>
         <App />
+        <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
 
     </BrowserRouter>
